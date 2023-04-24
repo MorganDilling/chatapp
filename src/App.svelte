69526e90ef93a272bdaf32be6ed00c7eb7cb2602
0lir055 <olirossxiv@gmail.com>
2023-04-24 13:11:58 +0100

    refactor: :coffin: Removed pocketbase within the app.svelte area

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,6 @@
   import app from './components/app.svelte';
   import register from './components/register.svelte';
   import login from './components/login.svelte';
-  import pocketbase from 'pocketbase';
-  const client = pocketbase({
-    apiKey: 'API_KEY',
-    databaseUrl: 'DATABASE_URL',
-  });
 </script>
 
 <Router>
