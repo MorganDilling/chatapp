83d70f4e7495e973072b1e9210304003d1b7e88d
0lir055 <olirossxiv@gmail.com>
2023-04-24 11:42:57 +0100

    build(dep): Implemented pocket base

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,13 @@
-<script>
+<script lang="ts">
   import { Router, Route } from 'svelte-routing';
   import app from './components/app.svelte';
   import register from './components/register.svelte';
   import login from './components/login.svelte';
+  import pocketbase from 'pocketbase';
+  const client = pocketbase({
+    apiKey: 'API_KEY',
+    databaseUrl: 'DATABASE_URL',
+  });
 </script>
 
 <Router>
