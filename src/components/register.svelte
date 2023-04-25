fc97f1aeb104775af99e87e3793f8db06772ef22
Morgan Dilling <morgandilling171@gmail.com>
2023-04-25 14:42:44 +0100

    feat: Updated register page with new Input component

--- src/components/register.svelte
+++ src/components/register.svelte
@@ -1,98 +1,55 @@
-<script lang=ts>
-	import {Link} from 'svelte-routing';
+<script lang="ts">
+  import { Link } from 'svelte-routing';
+  import Input from './Input.svelte';
 </script>
-<main >
-	<h1>ChatApp</h1>
-	<form>
-		<input type="text" class="username" name="username" placeholder="USERNAME" >
-		<input type="password" class="password"  name="password" placeholder="PASSWORD">
-		<button>REGISTER</button>
-	</form>
-	<p>Already have an account?</p>
-	<Link to="/login">Log in here.</Link>
+
+<main>
+  <h1>ChatApp</h1>
+  <form>
+    <Input type="text" name="username" placeholder="username" />
+    <Input type="password" name="password" placeholder="password" />
+    <button>REGISTER</button>
+  </form>
+  <p>Already have an account?</p>
+  <Link to="/login">Log in here.</Link>
 </main>
+
 <style lang="scss">
-	@use '../lib/styles/colours.scss' as colours;
+  @use '../lib/styles/colours.scss' as colours;
 
+  main {
+    background-color: colours.$primary;
+    width: 100vw;
+    height: 100vh;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  button {
+    width: 298px;
+    height: 70px;
+    font-size: 36px;
+    padding: 0px;
+    margin-top: 15px;
+    border: none;
+    color: colours.$primary;
+    font-weight: bold;
+  }
 
-	main {
-		background-color: colours.$primary;
-		width: 100vw;
-		height: 100vh;
-		text-align: center;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex-direction: column;
-		
-	}
+  p {
+    width: 298px;
+    position: relative;
+    display: block;
+    margin: 0 auto;
+    font-size: 24px;
+    color: white;
+  }
 
-	.username {
-		display: block;
-		width: 298px;
-		border-left: none;
-		border-top: none;
-		border-right: none;
-		border-width:3px;
-		border-color: white;
-		position: relative;
-		display:block;
-		margin : 0 auto;
-		font-size: 36px;
-		padding: 0px;
-		margin-top: 16px;
-		margin-bottom: 39px;
-		outline: none;
-		background-color: colours.$primary;
-		color: white;
-
-	}
-
-	.password {
-		display: block;
-		width: 298px;
-		border-left: none;
-		border-top: none;
-		border-right: none;
-		border-width:3px;
-		border-color: white;
-		position: relative;
-		display:block;
-		margin : 0 auto;
-		font-size: 36px;
-		padding: 0px;
-		margin-bottom: 39px;
-		outline: none;
-		background-color: colours.$primary;
-		color: white;
-
-		
-	}
-	button {
-		width: 298px;
-		height: 70px;
-		font-size: 36px;
-		padding: 0px;
-		margin-top: 15px;
-		border:none;
-		color: colours.$primary;
-		font-weight: bold;
-	}
-
-	p {
-		width: 298px;
-		position: relative;
-		display: block;
-		margin :0 auto;
-		font-size: 24px;
-		color: white;
-		
-	}
-
-	h1 {
-		font-size: 96px;
-		margin: 0px;
-		color: white
-	}
-
+  h1 {
+    font-size: 96px;
+    margin: 0px;
+    color: white;
+  }
 </style>
