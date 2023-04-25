57ee8135298a5a46942ed251a96df738a8a983a8
Philip Looker <philiplooker63@gmail.com>
2023-04-25 11:53:01 +0100

    style: :lipstick: changes to app.svelte
    added path to press a link that opens the register site
    
    no issues

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-  import { Router, Route } from 'svelte-routing';
+  import { Router, Route, Link } from 'svelte-routing';
   import app from './components/app.svelte';
   import register from './components/register.svelte';
   import login from './components/login.svelte';
 </script>
 
 <Router>
+  <Route path="/">
+    <Link to="/register">register</Link>  
+  </Route>
+
   <Route path="app" component={app} />
   <Route path="register" component={register} />
   <Route path="login" component={login} />
