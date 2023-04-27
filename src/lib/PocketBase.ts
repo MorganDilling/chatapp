a93c9f00f37633027cea7f7caf3af841ef5a6d3e
Joe Whitehouse <joewhitehouse1024@gmail.com>
2023-04-27 11:51:21 +0100

    feat: :sparkles: added error handling

--- src/lib/PocketBase.ts
+++ src/lib/PocketBase.ts
@@ -1,4 +1,5 @@
 import PocketBase from 'pocketbase';
+import { select_multiple_value } from 'svelte/internal';
 import { writable } from 'svelte/store';
 
 export const pb = new PocketBase("https://chatapp-api.morgandilling.dev")
@@ -6,12 +7,21 @@ export const pb = new PocketBase("https://chatapp-api.morgandilling.dev")
 export const autheduser = writable(pb.authStore.model)
 
 export const login = async (username: string, password: string) => {
-  await pb.collection('users').authWithPassword(username, password);
+  try {
+    return await pb.collection('users').authWithPassword(username, password);
+  } catch (e) {
+    return null
+  }
 };
 
 export const logout = async () => {
   pb.authStore.clear();
 };
+
+export const register = async (username: string, password: string) => {
+  await pb.collection('users').create({username, password, passwordConfirm: password})
+};
+
 
 pb.authStore.onChange((auth) => {
   autheduser.set(pb.authStore.model);
