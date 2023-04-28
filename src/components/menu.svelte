d234a3584f16ad9140542083023ca3f6b189502c
Philip Looker <philiplooker63@gmail.com>
2023-04-28 09:52:14 +0100

    feat: :sparkles: Add application menu

---
+++ src/components/menu.svelte
@@ -0,0 +1,57 @@
+<body>
+    <main>
+        <button>CHANGE USERNAME</button>
+        <button>CHANGE PASSWORD</button>
+        <button>CHANGE PROFILE PIC</button>
+        <button class="small redbutton">LOG OUT</button>
+        <button class="redbutton bigspace">DELETE ACCOUNT</button>
+        <button class="small" >CLOSE</button>
+    </main>    
+</body>
+
+
+
+<style lang="scss">
+    @use '../lib/styles/colours.scss' as colours;
+    body {
+        background-color: grey;
+        
+    }
+    main {
+        float: right;
+        background-color: white;
+        width: 315.01px;
+        height: 100vh;
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+    button {
+        display: block;
+        text-align: center;  
+        width: 75%;
+        height: 82.5px;
+        font-size: 26.25px;
+        font-weight: 700;  
+        color: white;   
+        background-color: colours.$primary; 
+        border: none;
+        outline: none;
+        margin-bottom: 23px;
+    }
+    .small{
+        height: 54px;
+        display: block;
+        text-align: center;
+
+    }
+    .redbutton {
+        background-color: colours.$error;
+   
+    }
+    .bigspace{
+        margin-bottom: 154.6875px;
+    }
+</style>
